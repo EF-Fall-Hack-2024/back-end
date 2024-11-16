7c9dd182bd151303cfb730fad30c47c8981e3368
Daniel Ik <codingeinstein@gmail.com>
2024-11-16 16:51:58 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const { OAuth2Client } = require('google-auth-library');
+const oauth2Client = new OAuth2Client()
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// 1. Call the Google SDK from the frontend using whatever frontend
+//2. Extract the code or access token and send to your backend for verification.
+//3. Use your backend Google api to verify the code or token.
+//4. If verified, sign them in the backend and then send a response to frontend
+
+  app.post('/google/auth', async (req, res) => {
+    try {
+// get the code from frontend
+      const code = req.headers.authorization;
+      console.log('Authorization Code:', code);
+
+      // Exchange the authorization code for an access token
+      // TODO: Use env.
+      
+      const response = await axios.post(
+        'https://oauth2.googleapis.com/token',
+        {
+          code,
+          client_id: '104012653988-qv7tq5k1c0p42mqt3gh6ul87m2bao45o.apps.googleusercontent.com',
+          client_secret: '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',
+          redirect_uri: 'postmessage',
+          grant_type: 'authorization_code'
+        }
+      );
+      const accessToken = response.data.access_token;
+      console.log('Access Token:', accessToken);
+
+      // Fetch user details using the access token
+      const userResponse = await axios.get(
+        'https://www.googleapis.com/oauth2/v3/userinfo',
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        }
+      );
+      const userDetails = userResponse.data;
+      console.log('User Details:', userDetails);
+
+      // Process user details and perform necessary actions
+
+      res.status(200).json({ message: 'Authentication successful' });
+    } catch (error) {
+      console.error('Error saving code:', error);
+      res.status(500).json({ message: 'Failed to save code' });
+    }
+  });
+
+
+app.listen(4000, () => {
+    console.log('Server running on port 4000');
+});
